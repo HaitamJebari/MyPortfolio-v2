c24c9137de67864f1576040df43feba5c9ad3fa6
haitamjebari <haitamjebari02@gmail.com>
2026-02-08 23:08:03 +0100

    Add Google Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App.jsx"
-import "./index.css"
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-9XKCY9XF93");
+ReactGA.send("pageview");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-)
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
